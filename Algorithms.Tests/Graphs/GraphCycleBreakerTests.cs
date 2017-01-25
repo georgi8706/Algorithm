0aa65ab6d1515d8564972bd47d25357e1350db3b
georgi8706 <georgi_vz@abv.bg>
2017-01-26 01:49:42 +0200

    Graph cycle breaker algorithm added.

---
+++ Algorithms.Tests/Graphs/GraphCycleBreakerTests.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using Algorithms.Graphs_AdjacencyLists;
+
+namespace Algorithms.Tests.Graphs
+{
+    [TestClass]
+    public class GraphBreakCyclesTests
+    {
+        [TestMethod]
+        public void GraphSalaryTest()
+        {
+            Graph graph = CreateSimpleGraph();
+
+            var salaryCalculator = new GraphSalaryCalculator();
+            salaryCalculator.SetGraph(graph);
+
+            List<int> salaries = salaryCalculator.Calculate(graph);
+
+            var expectedSalaries = new List<int>() { 1, 6, 3, 1, 4, 2 };
+
+            CollectionAssert.AreEqual(salaries, expectedSalaries);
+        }
+
+        private Graph CreateSimpleGraph()
+        {
+            var graph = new Graph();
+
+            var dependencyTable = new List<string>{"NNNNNN",
+                                                   "YNYNNY",
+                                                   "YNNNNY",
+                                                   "NNNNNN",
+                                                   "YNYNNN",
+                                                   "YNNYNN"};
+
+            graph.CreateFromDependencyTable(dependencyTable);
+
+            return graph;
+        }
+    }
+}
---
+++ Graphs/GraphCyclesBreaker.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Algorithms.Graphs_AdjacencyLists;
+
+namespace Algorithms
+{
+    public class GraphCyclesBreaker
+    {
+        Graph _graph;
+        List<int> _visitedNodes = new List<int>();
+
+        public List<KeyValuePair<int, int>> BreakCycles()
+        {
+            var removedEdges = new List<KeyValuePair<int, int>>();
+
+            return removedEdges;
+        }
+
+        public void SetGraph(Graph graph)
+        {
+            _graph = graph;
+        }
+    }
+}
