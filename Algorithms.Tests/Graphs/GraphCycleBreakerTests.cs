94b4190ef5fa7904bdb863ef4d4767a672fcbb27
georgi8706 <georgi_vz@abv.bg>
2017-01-27 02:47:27 +0200

    Breaking an cycle graph into acyclic one.

--- Algorithms.Tests/Graphs/GraphCycleBreakerTests.cs
+++ Algorithms.Tests/Graphs/GraphCycleBreakerTests.cs
@@ -6,35 +6,90 @@ using Algorithms.Graphs_AdjacencyLists;
 namespace Algorithms.Tests.Graphs
 {
     [TestClass]
-    public class GraphBreakCyclesTests
+    public class GraphCycleBreakerTests
     {
         [TestMethod]
-        public void GraphSalaryTest()
+        public void GraphBreakCycleTest1()
         {
-            Graph graph = CreateSimpleGraph();
+            Graph graph = CreateSimpleGraph1();
 
-            var salaryCalculator = new GraphSalaryCalculator();
-            salaryCalculator.SetGraph(graph);
+            var graphCycleBreaker = new GraphCyclesBreaker();
+            graphCycleBreaker.SetGraph(graph);
 
-            List<int> salaries = salaryCalculator.Calculate(graph);
+            List<Tuple<int, int>> removedEdges = graphCycleBreaker.BreakCycles();
 
-            var expectedSalaries = new List<int>() { 1, 6, 3, 1, 4, 2 };
+            var expectedRemovedEdges = new List<Tuple<int, int>>();
+            expectedRemovedEdges.Add(new Tuple<int, int>(1, 2));
+            expectedRemovedEdges.Add(new Tuple<int, int>(6, 7));
 
-            CollectionAssert.AreEqual(salaries, expectedSalaries);
+            CollectionAssert.AreEqual(removedEdges, expectedRemovedEdges);
         }
 
-        private Graph CreateSimpleGraph()
+        [TestMethod]
+        public void GraphBreakCycleTest2()
+        {
+            Graph graph = CreateSimpleGraph2();
+
+            var graphCycleBreaker = new GraphCyclesBreaker();
+            graphCycleBreaker.SetGraph(graph);
+
+            List<Tuple<int, int>> removedEdges = graphCycleBreaker.BreakCycles();
+
+            var expectedRemovedEdges = new List<Tuple<int, int>>();
+
+            expectedRemovedEdges.Add(new Tuple<int, int>(8, 9)); // A-Z
+            expectedRemovedEdges.Add(new Tuple<int, int>(8, 9)); // A-Z
+            expectedRemovedEdges.Add(new Tuple<int, int>(12, 10)); // B-F
+            expectedRemovedEdges.Add(new Tuple<int, int>(11, 10)); // E-F
+            expectedRemovedEdges.Add(new Tuple<int, int>(7, 5)); // I-L
+            expectedRemovedEdges.Add(new Tuple<int, int>(1, 0)); // J-K
+            expectedRemovedEdges.Add(new Tuple<int, int>(5, 3)); // L-N
+
+            CollectionAssert.AreEqual(removedEdges, expectedRemovedEdges);
+        }
+
+        private Graph CreateSimpleGraph1()
         {
             var graph = new Graph();
 
-            var dependencyTable = new List<string>{"NNNNNN",
-                                                   "YNYNNY",
-                                                   "YNNNNY",
-                                                   "NNNNNN",
-                                                   "YNYNNN",
-                                                   "YNNYNN"};
+            // Component 1
+            graph.InsertNode(0, new int[] { 1, 4, 3 }, 1);
+            graph.InsertNode(1, new int[] { 0, 2 }, 2);
+            graph.InsertNode(2, new int[] { 1, 4 }, 3);
+            graph.InsertNode(3, new int[] { 0 }, 4);
+            graph.InsertNode(4, new int[] { 0, 2 }, 5);
 
-            graph.CreateFromDependencyTable(dependencyTable);
+            // Component 2
+            graph.InsertNode(5, new int[] { 6, 7 }, 6);
+            graph.InsertNode(6, new int[] { 5, 7 }, 7);
+            graph.InsertNode(7, new int[] { 5, 6 }, 8);
+
+            return graph;
+        }
+
+        private Graph CreateSimpleGraph2()
+        {
+            var graph = new Graph();
+
+            // Component 1
+            graph.InsertNode(0, new int[] { 1, 2 }, 'K');
+            graph.InsertNode(1, new int[] { 0, 3 }, 'J');
+            graph.InsertNode(2, new int[] { 0, 3, 4 }, 'X');
+            graph.InsertNode(3, new int[] { 1, 2, 5, 6 }, 'N');
+            graph.InsertNode(4, new int[] { 2, 5 }, 'Y');
+            graph.InsertNode(5, new int[] { 3, 4, 7 }, 'L');
+            graph.InsertNode(6, new int[] { 3, 7 }, 'M');
+            graph.InsertNode(7, new int[] { 5 }, 'I');
+
+            // Component 2
+            graph.InsertNode(8, new int[] { 9 }, 'A');
+            graph.InsertNode(9, new int[] { 8 }, 'Z');
+
+            // Component 3
+            graph.InsertNode(10, new int[] { 11, 12, 13 }, 'F');
+            graph.InsertNode(11, new int[] { 10, 13 }, 'E');
+            graph.InsertNode(12, new int[] { 10, 13 }, 'B');
+            graph.InsertNode(13, new int[] { 11, 12, 13 }, 'P');
 
             return graph;
         }
--- Graphs/GraphCyclesBreaker.cs
+++ Graphs/GraphCyclesBreaker.cs
@@ -11,12 +11,43 @@ namespace Algorithms
     {
         Graph _graph;
         List<int> _visitedNodes = new List<int>();
+        List<Tuple<int, int>> _edges = new List<Tuple<int, int>>();
 
-        public List<KeyValuePair<int, int>> BreakCycles()
+        public List<Tuple<int, int>> BreakCycles()
         {
-            var removedEdges = new List<KeyValuePair<int, int>>();
+            var removedEdges = new List<Tuple<int, int>>();
+
+            foreach(var node in _graph.Nodes)
+            {
+                TraverseGraph(node.Index);
+            }
 
             return removedEdges;
+        }
+
+        /// <summary>
+        /// Traverses a graph by DFS recursively.
+        /// </summary>
+        private void TraverseGraph(int nodeIndex)
+        {
+            if (_visitedNodes.Contains(nodeIndex))
+            {
+                return;
+            }
+
+            _visitedNodes.Add(nodeIndex);
+
+            foreach (var child in _graph[nodeIndex].ChildrenIndexes)
+            {
+                bool isEdgeAlreadyAdded =_edges.FirstOrDefault(tuple => (tuple.Item1 == nodeIndex || tuple.Item1 == child) && (tuple.Item2 == nodeIndex || tuple.Item2 == child)) != null;
+
+                if (!isEdgeAlreadyAdded)
+                {
+                    _edges.Add(new Tuple<int, int>(nodeIndex, child));
+                }
+
+                TraverseGraph(child);
+            }
         }
 
         public void SetGraph(Graph graph)
