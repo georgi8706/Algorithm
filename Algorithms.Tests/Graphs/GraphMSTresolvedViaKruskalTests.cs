f6bae697e553633004de1448e7b6d906f4cab9d8
georgi8706 <georgi_vz@abv.bg>
2017-02-03 02:46:47 +0200

    Minimum Spanning Tree algorithm added implemented via Kruskal.

---
+++ Algorithms.Tests/Graphs/GraphMSTresolvedViaKruskalTests.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using Algorithms.Graphs_AdjacencyLists;
+
+namespace Algorithms.Tests.Graphs
+{
+    [TestClass]
+    public class GraphMSTresolvedViaKruskalTests
+    {
+        [TestMethod]
+        public void GraphMSTresolvedViaKruskalTest()
+        {
+            Graph graph = CreateSimpleGraph();
+
+            var mstFinder = new GraphMSTresolvedViaKruskal();
+            mstFinder.SetGraph(graph);
+
+            List<int> mstPath = mstFinder.FindMST();
+
+            var expectedMSTpath = new List<int>() { 1, 2, 3};
+
+            CollectionAssert.AreEqual(mstPath, expectedMSTpath);
+        }
+
+        private Graph CreateSimpleGraph()
+        {
+            var graph = new Graph();
+
+            // Component 1
+            graph.InsertNode(0, new int[] { 1, 4, 3 }, '1');
+            graph.InsertNode(1, new int[] { 0, 2 }, '2');
+            graph.InsertNode(2, new int[] { 1, 4 }, '3');
+            graph.InsertNode(3, new int[] { 0 }, '4');
+            graph.InsertNode(4, new int[] { 0, 2 }, '5');
+
+            // Component 2
+            graph.InsertNode(5, new int[] { 6, 7 }, '6');
+            graph.InsertNode(6, new int[] { 5, 7 }, '7');
+            graph.InsertNode(7, new int[] { 5, 6 }, '8');
+
+            return graph;
+        }
+    }
+}
---
+++ Graphs/GraphMSTresolvedViaKruskal.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Algorithms.Graphs_AdjacencyLists;
+
+namespace Algorithms
+{
+    // Minimum Spanning Tree (or forest, when we have more than one component in the graph)
+    public class GraphMSTresolvedViaKruskal
+    {
+        Graph _graph;
+        List<int> _visitedNodes = new List<int>();
+
+        List<Tuple<int, int>> _edges = new List<Tuple<int, int>>();
+
+        public List<int> FindMST()
+        {
+            List<int> mstPath = new List<int>();
+
+            return mstPath;
+        }
+
+        public void SetGraph(Graph graph)
+        {
+            _graph = graph;
+        }
+    }
+}
