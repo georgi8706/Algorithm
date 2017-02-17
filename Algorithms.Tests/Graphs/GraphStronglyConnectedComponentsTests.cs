6ac80deb4f9ff0701298de191019c74be73cbf5f
georgi8706 <georgi_vz@abv.bg>
2017-02-17 02:19:52 +0200

    updates

---
+++ Algorithms.Tests/Graphs/GraphStronglyConnectedComponentsTests.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using Algorithms.Graphs_AdjacencyLists;
+
+namespace Algorithms.Tests.Graphs
+{
+    [TestClass]
+    public class GraphStronglyConnectedComponentsTests
+    {
+        [TestMethod]
+        public void GraphStronglyConnectedComponentsTests1()
+        {
+            Graph graph = CreateSimpleGraph();
+
+            var stronglyConnectedComponentsFinder = new GraphStronglyConnectedComponents();
+            stronglyConnectedComponentsFinder.SetGraph(graph);
+
+            List<List<int>> components = stronglyConnectedComponentsFinder.FindStronglyConnectedComponents();
+
+            var component1 = new List<int>() { 5 };
+            var component2 = new List<int>() { 7, 12 };
+            var component3 = new List<int>() { 0, 1, 2, 6, 9, 13 };
+            var component4 = new List<int>() { 11 };
+            var component5 = new List<int>() { 8 };
+            var component6 = new List<int>() { 10 };
+            var component7 = new List<int>() { 3, 4 };
+
+            var expectedComponents = new List<List<int>>() { component1, component2, component3, component4, component5, component6, component7 };
+
+            CollectionAssert.AreEqual(components, expectedComponents);
+        }
+
+        private Graph CreateSimpleGraph()
+        {
+            var graph = new Graph();
+
+            graph.InsertNode(0, new int[] { 1, 11, 13 });
+            graph.InsertNode(1, new int[] { 6 });
+            graph.InsertNode(2, new int[] { 0 });
+            graph.InsertNode(3, new int[] { 4 });
+            graph.InsertNode(4, new int[] { 3, 6 });
+            graph.InsertNode(5, new int[] { 13 });
+            graph.InsertNode(6, new int[] { 0, 11 });
+            graph.InsertNode(7, new int[] { 12 });
+            graph.InsertNode(8, new int[] { 6, 11 });
+            graph.InsertNode(9, new int[] { 0 });
+            graph.InsertNode(10, new int[] { 4, 6, 10 });
+            graph.InsertNode(11);
+            graph.InsertNode(12, new int[] { 7 });
+            graph.InsertNode(13, new int[] { 2, 9 });
+
+            return graph;
+        }
+    }
+}
---
+++ Graphs/GraphStronglyConnectedComponents.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Algorithms.Graphs_AdjacencyLists;
+using Algorithms.AllKind;
+
+namespace Algorithms
+{
+    public class GraphStronglyConnectedComponents
+    {
+        Graph _graph;
+
+        Stack<int> _componentPath;
+
+        private List<int> _visitedNodes = new List<int>();
+
+        public void SetGraph(Graph graph)
+        {
+            _componentPath = new Stack<int>();
+
+            _visitedNodes = new List<int>();
+
+            _graph = graph;
+        }
+
+        public List<List<int>> FindStronglyConnectedComponents()
+        {
+            var components = new List<List<int>>();
+
+            _visitedNodes.Clear();
+
+            //foreach (Node node in _graph.Nodes)
+            {
+                TraverseGraphUsingRecursion(5);
+            }
+
+            return components;
+        }
+
+        private bool TraverseGraphUsingRecursion(int nodeIndex)
+        {
+            if (_visitedNodes.Contains(nodeIndex))
+            {
+                return false;
+            }
+
+            _visitedNodes.Add(nodeIndex);
+
+            bool nonVisitedChildExists = false;
+
+            foreach (var child in _graph[nodeIndex].Children)
+            {
+                if (TraverseGraphUsingRecursion(child))
+                {
+                    nonVisitedChildExists = true;
+                }
+            }
+
+            if (!nonVisitedChildExists)
+            {
+                _componentPath.Push(nodeIndex);
+            }
+
+            return true;
+        }
+    }
+}
